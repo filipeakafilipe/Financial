105e3805d8b249e30e12c3d0066cd2fb6c916ce6
Filipe <filipeffb99@gmail.com>
2020-10-22 00:46:26 -0300

    Adiciona modelo de Cotna

---
+++ Models/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financial.Models
+{
+    public class Conta : IConta
+    {
+        #region Propriedades
+
+        public int Id { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string PacoteServico { get; set; }
+
+        #endregion
+
+        #region Construtores
+
+        public Conta()
+        {
+
+        }
+
+        #endregion
+
+        #region Métodos
+        
+        public decimal VisualizarSaldo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GerarSaldo(decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
